f1d05e5b83ae9b65e6949c00aa419bb45fd2365e
AndyJin <2358155969@qq.com>
2025-09-02 20:09:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/contexts/ThemeContext'
+import { UserProvider } from '@/contexts/UserContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="zh-CN" suppressHydrationWarning>
       <body className={`${inter.className} gradient-bg min-h-screen`}>
         <ThemeProvider>
+          <UserProvider>
           {children}
+          </UserProvider>
         </ThemeProvider>
       </body>
     </html>
