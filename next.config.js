93cd5d339a05f329e9d4ce2156350f174dc1f60d
AndyJin <2358155969@qq.com>
2025-09-05 14:26:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: 'https://api.andyjin.website/api/auth/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig 
